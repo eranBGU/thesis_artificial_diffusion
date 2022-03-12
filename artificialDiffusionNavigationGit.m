8cd3ab49aaa410c2addaf88e77ff4af6e5f1aeab
eranBGU <eranniss@post.bgu.ac.il>
2022-03-12 19:16:02 +0200

    making many concentration map.The code needs to be matched to a number of robots

--- artificialDiffusionNavigationGit.m
+++ artificialDiffusionNavigationGit.m
@@ -5,7 +5,7 @@ clc, clear, close all;
 %% Simulation Parameters
 Lx = 1;                             % plate width (m)
 Ly = 1;                             % plate length (m)
-nx = 100;                         % number of nodes in x direction
+nx = 80;                         % number of nodes in x direction
 ny = nx;                            % number of nodes in y direction 
 x = linspace(0,Lx,nx);
 y = linspace(0,Ly,ny);
@@ -24,19 +24,21 @@ T.conc = -10000;
 
 % Targets location
 T_loc(1,:) = [0.611 0.611];
-T_loc(2,:) = [0.111 0.611]; % Second target
-[~,T.locidx(1)] = min(abs(x-T_loc(1,1))); % x closest location in the mesh
-[~,T.locidx(2)] = min(abs(y-T_loc(1,2))); % y closest location in the mesh
-T.loc(1) = x(T.locidx(1));
-T.loc(2) = y(T.locidx(2));
+T_loc(2,:) = [0.611 0.111]; % Second target
+[~,T.locidx(:,1)] = min(abs(x-T_loc(:,1)),[],2); % x closest location in the mesh
+[~,T.locidx(:,2)] = min(abs(y-T_loc(:,2)),[],2); % y closest location in the mesh
+T.loc(:,1) = x(T.locidx(:,1));
+T.loc(:,2) = y(T.locidx(:,2));
+ntarget = size(T_loc,1);
 
 % Robots location
 R_loc = [0.311 0.411];
-[~,R.locidx(1)] = min(abs(x-R_loc(1))); % x closest location in the mesh
-[~,R.locidx(2)] = min(abs(y-R_loc(2))); % y closest location in the mesh
-R(1).loc(1) = x(R.locidx(1));
-R(1).loc(2) = y(R.locidx(2));
-number_robots = 1;
+R_loc(2,:) = [0.811 0.111]; % Second robot
+[~,R.locidx(:,1)] = min(abs(x-R_loc(:,1)),[],2); % x closest location in the mesh
+[~,R.locidx(:,2)] = min(abs(y-R_loc(:,2)),[],2); % y closest location in the mesh
+R.loc(:,1) = x(R.locidx(:,1));
+R.loc(:,2) = y(R.locidx(:,2));
+
 
 % Obstacles location
 O.borders = [0 0 Lx Lx ; 0 Ly Ly 0];
@@ -48,18 +50,22 @@ in_obs = inpolygon(X(:), Y(:), O(1).obs.Vertices(:,1),O(1).obs.Vertices(:,2));
 for iobsplot =2:length(O)
     in_obs = in_obs | inpolygon(X(:), Y(:), O(iobsplot).obs.Vertices(:,1),O(iobsplot).obs.Vertices(:,2));
 end
-% Concentration Map
-cmap = c_init.*ones(numel(x),numel(y),numel(number_robots));
-cmap(T.locidx(:,1),T.locidx(:,2))=T.conc;
 
 % Calculate all the indexes 
-calc_idx = ones(size(X));
-calc_idx(1:length(x),1) = 0;                % borders
-calc_idx(1:length(x),end) = 0;              % borders
-calc_idx(1,1:length(y)) = 0;                % borders
-calc_idx(end,1:length(y)) = 0;              % borders
-calc_idx(T.locidx(:,1),T.locidx(:,2)) = 0;  % target location
-calc_idx(in_obs)=0;                         % Obstacles
+calc_idx = ones(numel(x),numel(y),ntarget);
+calc_idx(1:length(x),1,:) = 0;                % borders
+calc_idx(1:length(x),end,:) = 0;              % borders
+calc_idx(1,1:length(y),:) = 0;                % borders
+calc_idx(end,1:length(y),:) = 0;              % borders
+calc_idx(repmat(in_obs,ntarget,1))=0;                         % Obstacles
+
+% Concentration Map
+cmap = c_init.*ones(numel(x),numel(y),ntarget);
+for imap = 1:ntarget
+cmap(T.locidx(imap,1),T.locidx(imap,2),imap)=T.conc;
+calc_idx(T.locidx(imap,1),T.locidx(imap,2),imap) = 0;  % target location
+end
+
 run_idx = find(calc_idx ~= 0);              %finding all the indexs without boundary condition
 
 %% Plot 2D Mesh
@@ -72,8 +78,8 @@ for iobsplot =1:length(O)
     plot(O(iobsplot).obs,'FaceColor','red');
 end
 
-plot(R.loc(1),R.loc(2),'go') % Robot location
-plot(T.loc(1),T.loc(2),'g*') % Target location
+plot(R.loc(:,1),R.loc(:,2),'go') % Robot location
+plot(T.loc(:,1),T.loc(:,2),'g*') % Target location
 axis equal
 xlabel('x[m]'); ylabel('y[m]');
 set(gcf,'Color','w')
@@ -81,7 +87,7 @@ hold off
 
 %% SS concentration calculation
 cnorm = zeros(1e+4,1); 
-cnorm(1) = norm(cmap);
+cnorm(1) = norm(cmap(:));
 isoutSS = 1;
 it = 1;
 norm_iteration = 10;
@@ -98,17 +104,18 @@ cmapvec(run_idx) = 0.25*(cmapvec(run_idx-1)+cmapvec(run_idx+1)+cmapvec(run_idx-n
     end
     it = it+1;
 end
-cmap = reshape(cmapvec,nx,ny);
+cmap = reshape(cmapvec,nx,ny,ntarget);
 
 %% Plot navigation map
 figure(2)
-surf(X,Y,cmap);
+surf(X,Y,cmap(:,:,1));
 % zlim([0.99999999e+4 1e+4])
 xlabel('x[m]'); ylabel('y[m]');zlabel('Conc[M]')
 
 %% Robot navigation
 isoutTarget = 1;
 ri = 1; %robot location iteration
+%% צריך לסדר ניווט של הרובוט השני למפה השנייה!!!!
 R(1).vinx = sub2ind(size(cmap),R.locidx(1),R.locidx(2)); %locidx(3) is the vectorize index of the robot
 dir.vec = [0; 1; -1; nx; nx+1; nx-1; -nx; -nx+1; -nx-1];
 while isoutTarget
@@ -125,7 +132,7 @@ end
 figure(3)
 set(gca,'ColorScale','log')
 colormap('white')
-contourf(X,Y,cmap,[10000,9999.9999999,9999.99999,9999.9999,9999.999,9999.99,  9999, 9990, 9900, 9000, 100,10,-3000, -10000])
+contourf(X,Y,cmap(:,:,1),[10000,9999.9999999,9999.99999,9999.9999,9999.999,9999.99,  9999, 9990, 9900, 9000, 100,10,-3000, -10000])
 hold on     
 gr=plot(X(R.vinx),Y(R.vinx),'k','LineWidth',1.5);
 xlabel('x[m]'); ylabel('y[m]')
